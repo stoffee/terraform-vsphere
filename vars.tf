bf3b1b4623c8fd87386a9630a35c3269344b369c
Chris Dunlap <chris@roadkill.org>
2019-10-19 16:17:48 -0700

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -9,3 +9,15 @@ variable "vsphere_password" {
 variable "vsphere_server" {
   description = "vSphere Server"
 }
+
+variable "tfe_hostname" {
+  description = "Terraform hostname"
+}
+
+variable "tfe_org" {
+  description = "Terraform Org"
+}
+
+variable "tfe_workspace" {
+  description = "Terraform Org"
+}
