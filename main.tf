94c4c54ae8a154765752f7f3c44b7fde26e5fde9
Chris Dunlap <chris@roadkill.org>
2019-10-19 16:25:25 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,12 +2,12 @@ module "example-server-linuxvm" {
   source        = "git::https://github.com/Terraform-VMWare-Modules/terraform-vsphere-vm.git"
 #  version       = "1.0.0"
   vmtemp        = "Ubuntu"
-  instances     = 1
+  instances     = 2
   vmname        = "cd-temp"
   vmrp          = "MainCluster/Resources"
   network_cards = ["VM Network"]
   ipv4 = {
-    "VM Network" = ["10.0.0.23"] # To use DHCP create Empty list for each instance
+    "VM Network" = ["",""] # To use DHCP create Empty list for each instance
   }
   dc        = "PacketDatacenter"
   datastore = "datastore1"
