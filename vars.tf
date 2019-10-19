33342e2fc6276ed510b80ea2e9add8ffa05290ac
Chris Dunlap <chris@roadkill.org>
2019-10-19 12:22:56 -0700

    Create vars.tf

---
+++ vars.tf
@@ -0,0 +1,11 @@
+variable "vsphere_user" {
+  description = "vSphere Username"
+}
+
+variable "vsphere_password" {
+  description = "vSphere Password"
+}
+
+variable "vsphere_server" {
+  description = "vSphere Server"
+}
