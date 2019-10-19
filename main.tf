4e90bdb2a11f5d2930ce573de6c71610ad8a0ca3
Chris Dunlap <chris@roadkill.org>
2019-10-19 12:11:50 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "example-server-linuxvm" {
-  source        = "https://github.com/Terraform-VMWare-Modules/terraform-vsphere-vm.git"
+  source        = "git::https://github.com/Terraform-VMWare-Modules/terraform-vsphere-vm.git"
   version       = "1.0.0"
   vmtemp        = "Ubuntu"
   instances     = 1
